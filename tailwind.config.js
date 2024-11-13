5b3605edad9506aabcdefbf2908f6b3052555af1
ndant-code <raja.farhan.alwanda@gmail.com>
2024-11-13 11:22:32 +0700

    belajar transformasi, transisi dan animasi

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
       fontFamily: {
         Geist: ["Geist"],
       },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        wiggle: "wiggle 2s linear infinite",
+      },
+      // * menambahkan bentuk animasi
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-20deg)" },
+          "50%": { transform: "rotate(20deg)" },
+        },
+      },
     },
   },
   plugins: [],
